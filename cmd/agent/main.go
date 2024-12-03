d2d1ba978f010dfecdb0ebf3717848f6f24011db
aleksei <lao.al@yandex.ru>
2024-12-03 20:59:52 +0000

    iter4 with flags , to do config struct

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/4aleksei/metricscum/internal/agent/gather"
 	"github.com/4aleksei/metricscum/internal/agent/handlers"
@@ -15,11 +19,39 @@ func main() {
 	}
 }
 
+type NetAddress struct {
+	Host string
+	Port int
+}
+
+func (o *NetAddress) String() string {
+	return fmt.Sprintf("%s:%d", o.Host, o.Port)
+}
+
+func (o *NetAddress) Set(flagValue string) error {
+	valstr := strings.Split(flagValue, ":")
+	o.Host = valstr[0]
+	o.Port, _ = strconv.Atoi(valstr[1])
+	return nil
+}
+
 func run() error {
 	//	cfg := config.GetConfig()
+	addr := new(NetAddress)
+	addr.Host = "localhost"
+	addr.Port = 8080
+
+	_ = flag.Value(addr)
+
+	flag.Var(addr, "a", "Net address host:port")
+
+	reportInterval := flag.Uint("r", 10, "reportInterval")
+	pollInterval := flag.Uint("p", 2, "pollInterval")
+
+	flag.Parse()
 
 	store := repository.NewStoreMux()
 	metricsService := service.NewHandlerStore(store)
-	go gather.MainGather(metricsService)
-	return handlers.MainHTTPClient(metricsService)
+	go gather.MainGather(metricsService, *pollInterval)
+	return handlers.MainHTTPClient(metricsService, addr.String(), *reportInterval)
 }
