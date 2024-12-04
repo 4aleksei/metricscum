6584d5c35d30bcde5bae9af4c684c724b02629b4
aleksei <lao.al@yandex.ru>
2024-12-04 03:02:31 +0000

    iter5 new few chi test

--- internal/server/handlers/handlers_test.go
+++ internal/server/handlers/handlers_test.go
@@ -8,9 +8,26 @@ import (
 	"github.com/4aleksei/metricscum/internal/server/service"
 	"github.com/stretchr/testify/assert"
 
-	// "github.com/stretchr/testify/require"
+	"io"
+
 	"github.com/4aleksei/metricscum/internal/common/repository"
+	"github.com/stretchr/testify/require"
 )
+
+func testRequest(t *testing.T, ts *httptest.Server, method,
+	path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := ts.Client().Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
 
 func Test_handlers_mainPageCounter(t *testing.T) {
 
@@ -23,6 +40,11 @@ func Test_handlers_mainPageCounter(t *testing.T) {
 		url    string
 	}
 	store := service.NewHandlerStore(repository.NewStore()) //
+	h := &handlers{
+		store: store,
+	}
+	ts := httptest.NewServer(newRouter(h))
+	defer ts.Close()
 
 	tests := []struct {
 		name string
@@ -32,21 +54,26 @@ func Test_handlers_mainPageCounter(t *testing.T) {
 		{name: "Test No1", req: request{method: http.MethodPost, url: "/update/gauge/test1/10"}, want: want{statusCode: http.StatusOK, contentType: "text/plain; charset=utf-8"}},
 
 		{name: "Test No2", req: request{method: http.MethodPost, url: "/update/counter/test2/10"}, want: want{statusCode: http.StatusOK, contentType: "text/plain; charset=utf-8"}},
+
+		{name: "Test No3", req: request{method: http.MethodPost, url: "/update/counter/"}, want: want{statusCode: http.StatusNotFound, contentType: "text/plain; charset=utf-8"}},
+		{name: "Test No4", req: request{method: http.MethodPost, url: "/update/unknown/test3/10"}, want: want{statusCode: http.StatusBadRequest, contentType: "text/plain; charset=utf-8"}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := &handlers{
-				store: store,
-			}
-			request := httptest.NewRequest(tt.req.method, tt.req.url, nil)
-			w := httptest.NewRecorder()
-			h.mainPageCounter(w, request)
 
-			result := w.Result()
-			assert.Equal(t, tt.want.statusCode, result.StatusCode)
-			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
-			result.Body.Close()
+			resp, _ := testRequest(t, ts, tt.req.method, tt.req.url)
+			assert.Equal(t, tt.want.statusCode, resp.StatusCode)
+			assert.Equal(t, tt.want.contentType, resp.Header.Get("Content-Type"))
+			/*
+				request := httptest.NewRequest(tt.req.method, tt.req.url, nil)
+				w := httptest.NewRecorder()
+				h.mainPageCounter(w, request)
+
+				result := w.Result()
+				assert.Equal(t, tt.want.statusCode, result.StatusCode)
+				assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+				result.Body.Close()*/
 		})
 	}
 }
