8db9b2eecbd425dc6f93f202636b953289e2e683
4aleksei <38482411+4aleksei@users.noreply.github.com>
2025-03-09 17:19:36 +0300

    Iter work pool (#18)
    * iter14
    
    * fix tests )
    
    * fix Accepts
    
    * fix bug no sig in body with key in params
    
    * iter 15 workers pool
    
    * fix dead lock
    
    * fix by comments
    
    * fix by comments , name of func sleepC
    
    * new pool
    
    * change main to app
    
    * change main to app
    
    ---------
    
    Co-authored-by: aleksei <lao.al@yandex.ru>

---
+++ internal/agent/app/app.go
@@ -0,0 +1,63 @@
+package app
+
+import (
+	"time"
+
+	"github.com/4aleksei/metricscum/internal/agent/config"
+	"github.com/4aleksei/metricscum/internal/agent/gather"
+	"github.com/4aleksei/metricscum/internal/agent/gatherps"
+	"github.com/4aleksei/metricscum/internal/agent/handlers"
+	"github.com/4aleksei/metricscum/internal/agent/handlers/httpclientpool"
+	"github.com/4aleksei/metricscum/internal/agent/service"
+	"github.com/4aleksei/metricscum/internal/common/logger"
+	"github.com/4aleksei/metricscum/internal/common/repository/memstoragemux"
+	"github.com/4aleksei/metricscum/internal/common/utils"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+)
+
+func registerSetLoggerLevel(ll *logger.Logger, cfg *config.Config, lc fx.Lifecycle) {
+	ll.SetLevel(cfg.Lcfg.Level)
+	lc.Append(utils.ToHook(ll))
+}
+
+func registerRunnersGather(gg *gather.AppGather, lc fx.Lifecycle) {
+	lc.Append(utils.ToHook(gg))
+}
+
+func registerRunnersGatherps(gg *gatherps.AppGatherMem, lc fx.Lifecycle) {
+	lc.Append(utils.ToHook(gg))
+}
+
+func registerRunnersSender(cc *handlers.App, lc fx.Lifecycle) {
+	lc.Append(utils.ToHook(cc))
+}
+
+func SetupFX() *fx.App {
+	app := fx.New(
+		fx.Supply(logger.Config{Level: "debug"}),
+		fx.StopTimeout(1*time.Minute),
+		fx.Provide(
+			logger.NewLogger,
+			config.GetConfig,
+			fx.Annotate(memstoragemux.NewStoreMux,
+				fx.As(new(service.AgentMetricsStorage))),
+			service.NewHandlerStore,
+			gather.NewAppGather,
+			gatherps.NewGather,
+			httpclientpool.NewHandler,
+			handlers.NewApp,
+		),
+
+		fx.WithLogger(func(log *logger.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log.L}
+		}),
+		fx.Invoke(
+			registerSetLoggerLevel,
+			registerRunnersSender,
+			registerRunnersGather,
+			registerRunnersGatherps,
+		),
+	)
+	return app
+}
