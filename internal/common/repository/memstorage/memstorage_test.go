fea91e2af78dc9400d54bdc10c5a20336fbe430f
aleksei <lao.al@yandex.ru>
2025-04-08 16:33:53 +0000

    test 24

--- internal/common/repository/memstorage/memstorage_test.go
+++ internal/common/repository/memstorage/memstorage_test.go
@@ -5,12 +5,31 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/4aleksei/metricscum/internal/common/models"
 	"github.com/4aleksei/metricscum/internal/common/repository/valuemetric"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_Add(t *testing.T) {
-	// Add(ctx context.Context, name string, val valuemetric.ValueMetric) (valuemetric.ValueMetric, error)
+func Test_PingContext(t *testing.T) {
+	n := NewStore()
+	tests := []struct {
+		name string
+		want error
+	}{
+		{name: "Test Ping", want: nil},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := n.PingContext(context.Background())
+			if got != tt.want {
+				t.Errorf("PingContext = %v ", got)
+			}
+		})
+	}
+}
+
+func Test_Add(t *testing.T) {
 	n := NewStore()
 	vF := valuemetric.ConvertToFloatValueMetric(55.55)
 	vFF := valuemetric.ConvertToFloatValueMetric(77.77)
@@ -33,12 +52,89 @@ func Test_Add(t *testing.T) {
 			got, gotErr := n.Add(context.Background(), tt.valName, tt.val)
 			if gotErr != nil {
 				if !errors.Is(gotErr, tt.wantErr) {
-					t.Errorf("ConvertToValueMetricInt = %v, want_err %v ", gotErr, tt.wantErr)
+					t.Errorf("Add = %v, want_err %v ", gotErr, tt.wantErr)
 				}
 			} else {
 				if got != tt.wantVal {
-					t.Errorf("ConvertToValueMetricInt = %v, want_val %v ", got, tt.wantVal)
+					t.Errorf("Add = %v, want_val %v ", got, tt.wantVal)
 				}
+			}
+		})
+	}
+}
+
+func Test_Get(t *testing.T) {
+	n := NewStore()
+	vF := valuemetric.ConvertToFloatValueMetric(55.55)
+	vI := valuemetric.ConvertToIntValueMetric(55)
+	nameOfTest1 := "Test1"
+	nameOfTest2 := "Test2"
+	n.Add(context.Background(), nameOfTest1, *vI)
+	n.Add(context.Background(), nameOfTest2, *vF)
+
+	tests := []struct {
+		name    string
+		valName string
+		wantVal valuemetric.ValueMetric
+		wantErr error
+	}{
+		{name: "Test Get Int", valName: nameOfTest1, wantVal: *vI, wantErr: nil},
+		{name: "Test Get Float", valName: nameOfTest2, wantVal: *vF, wantErr: nil},
+		{name: "Test Get NotFound", valName: "testNotFound", wantVal: *vF, wantErr: ErrNotFoundName},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := n.Get(context.Background(), tt.valName)
+			if gotErr != nil {
+				if !errors.Is(gotErr, tt.wantErr) {
+					t.Errorf("Get = %v, want_err %v ", gotErr, tt.wantErr)
+				}
+			} else {
+				if got != tt.wantVal {
+					t.Errorf("Get = %v, want_val %v ", got, tt.wantVal)
+				}
+			}
+		})
+	}
+}
+
+func Test_AddMulti(t *testing.T) {
+	n := NewStore()
+	var a int64 = 100
+	b := 100.1
+	modval := make([]models.Metrics, 0)
+	modval = append(modval, models.Metrics{ID: "Test1", MType: "counter", Delta: &a})
+	modval = append(modval, models.Metrics{ID: "Test2", MType: "gauge", Value: &b})
+
+	modval2 := make([]models.Metrics, 0)
+	modval2 = append(modval2, models.Metrics{ID: "Test3", MType: "Badcounter", Delta: &a})
+
+	modval3 := make([]models.Metrics, 0)
+	modval3 = append(modval3, models.Metrics{ID: "", MType: "counter", Delta: &a})
+
+	modval4 := make([]models.Metrics, 0)
+	modval4 = append(modval4, models.Metrics{ID: "Test4", MType: "counter"})
+
+	tests := []struct {
+		name    string
+		val     []models.Metrics
+		wantVal []models.Metrics
+		wantErr error
+	}{
+		{name: "Test AddMulti", val: modval, wantVal: modval, wantErr: nil},
+		{name: "Test AddMulti Error Kind", val: modval2, wantVal: modval2, wantErr: valuemetric.ErrBadTypeValue},
+		{name: "Test AddMulti Error Name", val: modval3, wantVal: modval3, wantErr: ErrBadName},
+		{name: "Test AddMulti Error Val", val: modval4, wantVal: modval4, wantErr: valuemetric.ErrBadValue},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := n.AddMulti(context.Background(), tt.val)
+			if gotErr != nil {
+				if !errors.Is(gotErr, tt.wantErr) {
+					t.Errorf("AddMulti = %v, want_err %v ", gotErr, tt.wantErr)
+				}
+			} else {
+				assert.ElementsMatch(t, got, tt.wantVal)
 			}
 		})
 	}
